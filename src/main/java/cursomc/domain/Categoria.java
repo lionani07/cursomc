bf447a5bb234c7ed1057cc733e1b24e57dbeeaf4
lionani07 <lionani07@gmail.com>
2020-08-22 22:18:15 -0300

    configure h2 database and refactoring domain class

--- src/main/java/cursomc/domain/Categoria.java
+++ src/main/java/cursomc/domain/Categoria.java
@@ -2,11 +2,19 @@ package cursomc.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Builder(toBuilder = true)
 @Value
 @EqualsAndHashCode(of = "id")
+@Entity
 public class Categoria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 }
