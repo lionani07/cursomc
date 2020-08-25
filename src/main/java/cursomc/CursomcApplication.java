b59471be711d6db5e16b6f6af1eaa3c885be3c4a
lionani07 <lionani07@gmail.com>
2020-08-25 19:13:23 -0300

    insert categorias on start

--- src/main/java/cursomc/CursomcApplication.java
+++ src/main/java/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package cursomc;
 
+import cursomc.domain.Categoria;
+import cursomc.respositoires.CategoriaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class CursomcApplication {
+@RequiredArgsConstructor
+public class CursomcApplication implements CommandLineRunner {
+
+	private final CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var categoria1 = Categoria.builder().nome("Informática").build();
+		var categoria2 = Categoria.builder().nome("Escritório").build();
+
+		this.categoriaRepository.saveAll(List.of(categoria1, categoria2));
+
+	}
 }
--- src/main/java/cursomc/domain/Categoria.java
+++ src/main/java/cursomc/domain/Categoria.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@Builder(toBuilder = true)
 public class Categoria {
 
     @Id
